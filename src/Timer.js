d19125e93e9d75a7487c11496a2d34ca6eaf07e4
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-02-05 00:42:19 +0100

    Removed superfluous audio element

--- src/Timer.js
+++ src/Timer.js
@@ -158,11 +158,6 @@ const Timer = () => {
       <button id="break-increment" onClick={handleDecOrInc}>
         B+
       </button>
-      <audio
-        id="beep"
-        preload="auto"
-        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
-      ></audio>
     </div>
   );
 };
